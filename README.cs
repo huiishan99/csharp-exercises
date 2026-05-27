42f6846462bd0f2d68eb7af02d8ead274cef1b8e
Hui Shan <61934115+huiishan99@users.noreply.github.com>
2026-05-27 16:30:56 +0900

    Update README.cs

--- README.cs
+++ README.cs
@@ -1,101 +1,305 @@
 using System.Collections;
 using UnityEngine;
 
-public class KinemaMockPopupController : MonoBehaviour
+public enum KinemaMockDisplayMode
 {
-    [SerializeField] private GameObject popupObject;
-    [SerializeField] private float autoCloseSeconds = 10f;
+    Close,
+    Opening,
+    Full,
+    Half,
+    RearView
+}
 
-    private Coroutine autoCloseCoroutine;
+public class KinemaMockDisplayController : MonoBehaviour
+{
+    [Header("Root")]
+    [SerializeField] private GameObject screenViewportRoot;
+    [SerializeField] private GameObject sourcePanelObject;
 
-    public bool IsVisible
+    [Header("Managers")]
+    [SerializeField] private DemoScreenViewport screenViewport;
+    [SerializeField] private DemoPageSwitcher pageSwitcher;
+    [SerializeField] private DemoSourcePanel sourcePanel;
+    [SerializeField] private KinemaMockPopupController popupController;
+
+    [Header("Page")]
+    [SerializeField] private DemoPageId openingPage = DemoPageId.Welcome;
+    [SerializeField] private DemoPageId drivePage = DemoPageId.NormalDrive;
+    [SerializeField] private DemoPageId rearPage = DemoPageId.RearView;
+
+    [Header("Parking")]
+    [SerializeField] private DemoSourceId parkingDefaultSource = DemoSourceId.Setting;
+
+    [Header("Opening")]
+    [SerializeField] private float openingDuration = 3.5f;
+
+    public KinemaMockDisplayMode CurrentDisplayMode { get; private set; }
+    public bool IsIgnOn { get; private set; }
+
+    private Coroutine openingCoroutine;
+
+    private void Start()
     {
-        get
+        ApplyCloseMode();
+    }
+
+    public void ToggleIgn()
+    {
+        if (IsIgnOn)
         {
-            return popupObject != null && popupObject.activeSelf;
+            IgnOff();
+            return;
         }
+
+        IgnOn();
     }
 
-    private void Awake()
+    public void IgnOn()
     {
-        HidePopup();
+        if (IsIgnOn)
+        {
+            return;
+        }
+
+        IsIgnOn = true;
+        EnterOpeningMode();
     }
 
-    private void OnDisable()
+    public void IgnOff()
     {
-        StopAutoCloseTimer();
+        if (!IsIgnOn)
+        {
+            return;
+        }
+
+        IsIgnOn = false;
+        StopOpeningCoroutine();
+        ApplyCloseMode();
     }
 
-    public void TogglePopup()
+    public void ShiftP()
     {
-        if (IsVisible)
+        if (!CanAcceptShiftInput())
+        {
+            return;
+        }
+
+        EnterFullMode();
+    }
+
+    public void ShiftD()
+    {
+        if (!CanAcceptShiftInput())
+        {
+            return;
+        }
+
+        EnterHalfMode();
+    }
+
+    public void ShiftR()
+    {
+        if (!CanAcceptShiftInput())
+        {
+            return;
+        }
+
+        EnterRearViewMode();
+    }
+
+    public void ToggleAutoPopup()
+    {
+        if (!IsIgnOn)
+        {
+            return;
+        }
+
+        if (CurrentDisplayMode != KinemaMockDisplayMode.Full)
         {
             HidePopup();
             return;
         }
 
-        ShowPopup();
+        if (popupController != null)
+        {
+            popupController.TogglePopup();
+        }
     }
 
-    public void ShowPopup()
+    private bool CanAcceptShiftInput()
     {
-        if (popupObject == null)
+        if (!IsIgnOn)
         {
-            return;
+            return false;
         }
 
-        popupObject.SetActive(true);
-        RestartAutoCloseTimer();
-    }
-
-    public void HidePopup()
-    {
-        StopAutoCloseTimer();
-
-        if (popupObject == null)
+        if (CurrentDisplayMode == KinemaMockDisplayMode.Opening)
         {
-            return;
+            return false;
         }
 
-        popupObject.SetActive(false);
+        return true;
     }
 
-    public void NotifyUserOperation()
+    private void ApplyCloseMode()
     {
-        if (!IsVisible)
-        {
-            return;
-        }
+        CurrentDisplayMode = KinemaMockDisplayMode.Close;
 
-        RestartAutoCloseTimer();
-    }
-
-    private void RestartAutoCloseTimer()
-    {
-        StopAutoCloseTimer();
-
-        if (autoCloseSeconds <= 0f)
-        {
-            return;
-        }
-
-        autoCloseCoroutine = StartCoroutine(AutoCloseRoutine());
-    }
-
-    private IEnumerator AutoCloseRoutine()
-    {
-        yield return new WaitForSeconds(autoCloseSeconds);
         HidePopup();
+
+        if (sourcePanel != null)
+        {
+            sourcePanel.ResetFullSource(parkingDefaultSource);
+        }
+
+        SetActive(screenViewportRoot, false);
+        SetActive(sourcePanelObject, false);
     }
 
-    private void StopAutoCloseTimer()
+    private void EnterOpeningMode()
     {
-        if (autoCloseCoroutine == null)
+        StopOpeningCoroutine();
+
+        CurrentDisplayMode = KinemaMockDisplayMode.Opening;
+
+        HidePopup();
+
+        SetActive(screenViewportRoot, true);
+        SetActive(sourcePanelObject, false);
+
+        if (screenViewport != null)
+        {
+            screenViewport.SetMode(DemoScreenOpenMode.SemiOpen);
+        }
+
+        if (pageSwitcher != null)
+        {
+            pageSwitcher.ShowPage(openingPage);
+        }
+
+        openingCoroutine = StartCoroutine(OpeningRoutine());
+    }
+
+    private IEnumerator OpeningRoutine()
+    {
+        yield return new WaitForSeconds(openingDuration);
+
+        if (!IsIgnOn)
+        {
+            yield break;
+        }
+
+        if (CurrentDisplayMode != KinemaMockDisplayMode.Opening)
+        {
+            yield break;
+        }
+
+        EnterFullMode();
+    }
+
+    private void EnterFullMode()
+    {
+        CurrentDisplayMode = KinemaMockDisplayMode.Full;
+
+        SetActive(screenViewportRoot, true);
+        SetActive(sourcePanelObject, true);
+
+        if (screenViewport != null)
+        {
+            screenViewport.SetMode(DemoScreenOpenMode.FullOpen);
+        }
+
+        if (sourcePanel != null)
+        {
+            sourcePanel.ApplyVehicleMode(DemoVehicleMode.Parking);
+            sourcePanel.ShowCurrentFullSource();
+            return;
+        }
+
+        if (pageSwitcher != null)
+        {
+            pageSwitcher.ShowPage(DemoPageId.LightingColorChange);
+        }
+    }
+
+    private void EnterHalfMode()
+    {
+        CurrentDisplayMode = KinemaMockDisplayMode.Half;
+
+        HidePopup();
+
+        SetActive(screenViewportRoot, true);
+        SetActive(sourcePanelObject, true);
+
+        if (screenViewport != null)
+        {
+            screenViewport.SetMode(DemoScreenOpenMode.SemiOpen);
+        }
+
+        if (pageSwitcher != null)
+        {
+            pageSwitcher.ShowPage(drivePage);
+        }
+
+        if (sourcePanel != null)
+        {
+            sourcePanel.ApplyVehicleMode(DemoVehicleMode.Drive);
+        }
+    }
+
+    private void EnterRearViewMode()
+    {
+        CurrentDisplayMode = KinemaMockDisplayMode.RearView;
+
+        HidePopup();
+
+        SetActive(screenViewportRoot, true);
+        SetActive(sourcePanelObject, true);
+
+        if (screenViewport != null)
+        {
+            screenViewport.SetMode(DemoScreenOpenMode.SemiOpen);
+        }
+
+        if (pageSwitcher != null)
+        {
+            pageSwitcher.ShowPage(rearPage);
+        }
+
+        if (sourcePanel != null)
+        {
+            sourcePanel.ApplyVehicleMode(DemoVehicleMode.Rear);
+        }
+    }
+
+    private void HidePopup()
+    {
+        if (popupController == null)
         {
             return;
         }
 
-        StopCoroutine(autoCloseCoroutine);
-        autoCloseCoroutine = null;
+        popupController.HidePopup();
+    }
+
+    private void StopOpeningCoroutine()
+    {
+        if (openingCoroutine == null)
+        {
+            return;
+        }
+
+        StopCoroutine(openingCoroutine);
+        openingCoroutine = null;
+    }
+
+    private void SetActive(GameObject target, bool isActive)
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        target.SetActive(isActive);
     }
 }
