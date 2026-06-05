d69d61c0c4fc20f3d0719cf0b4ee014b99e59f3b
Hui Shan <61934115+huiishan99@users.noreply.github.com>
2026-06-05 14:25:40 +0900

    Update README.cs

--- README.cs
+++ README.cs
@@ -1,6 +1,32 @@
-public enum OledTouchSource
+public class OledTouchEvent
 {
-    Unknown,
-    Driver,
-    Passenger
+    public int X { get; private set; }
+    public int Y { get; private set; }
+    public OledTouchEventType EventType { get; private set; }
+    public OledTouchSource Source { get; private set; }
+
+    public OledTouchEvent(
+        int x,
+        int y,
+        OledTouchEventType eventType,
+        OledTouchSource source
+    )
+    {
+        X = x;
+        Y = y;
+        EventType = eventType;
+        Source = source;
+    }
+
+    public override string ToString()
+    {
+        return "source="
+            + Source
+            + " type="
+            + EventType
+            + " x="
+            + X
+            + " y="
+            + Y;
+    }
 }
