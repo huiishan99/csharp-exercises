c339bb00b753879f8bfe727e34acf5cd3fa3b4ca
Hui Shan <61934115+huiishan99@users.noreply.github.com>
2026-06-05 14:19:47 +0900

    Update README.cs

--- README.cs
+++ README.cs
@@ -1,11 +1,7 @@
-[ComImport]
-[Guid("0BD7A1BE-7A1A-44DB-8397-CC5392387B5E")]
-[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-public interface IAudioMMDeviceCollection
+public enum OledTouchEventType
 {
-    [PreserveSig]
-    int GetCount(out uint count);
-
-    [PreserveSig]
-    int Item(uint index, out IAudioMMDevice device);
+    Unknown,
+    Down,
+    Move,
+    Up
 }
