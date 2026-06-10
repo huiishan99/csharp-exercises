89528b4885880b72dbaf39c898033187b73e91b3
Hui Shan <61934115+huiishan99@users.noreply.github.com>
2026-06-10 14:37:14 +0900

    Update README.cs

--- README.cs
+++ README.cs
@@ -1,105 +1,20 @@
-using System;
-
-[Serializable]
-public class GuiEventMessageTypeEnvelope
+public class GuiEventMessage
 {
-    public string message_type;
-}
+    public string MessageType { get; private set; }
+    public string RawJson { get; private set; }
 
-[Serializable]
-public class GuiEventEmptyPayload
-{
-}
+    public GuiEventShifterPayload ShifterPayload { get; set; }
+    public GuiEventTouchPayload TouchPayload { get; set; }
+    public GuiEventHvacPayload HvacPayload { get; set; }
 
-[Serializable]
-public class GuiEventEmptyEnvelope
-{
-    public string message_type;
-    public GuiEventEmptyPayload payload;
-}
-
-[Serializable]
-public class GuiEventShifterPayload
-{
-    public string gear;
-    public string shift;
-    public string value;
-
-    public string GetGearText()
+    public GuiEventMessage(string messageType, string rawJson)
     {
-        if (!string.IsNullOrEmpty(gear))
-        {
-            return gear;
-        }
-
-        if (!string.IsNullOrEmpty(shift))
-        {
-            return shift;
-        }
-
-        if (!string.IsNullOrEmpty(value))
-        {
-            return value;
-        }
-
-        return "";
+        MessageType = messageType;
+        RawJson = rawJson;
     }
-}
 
-[Serializable]
-public class GuiEventShifterEnvelope
-{
-    public string message_type;
-    public GuiEventShifterPayload payload;
-}
-
-[Serializable]
-public class GuiEventTouchPayload
-{
-    public string source;
-    public int x;
-    public int y;
-
-    // backend文書側の可能性: event
-    public string @event;
-
-    // 口頭I/F側の可能性: event_type
-    public string event_type;
-
-    public string GetTouchEventText()
+    public override string ToString()
     {
-        if (!string.IsNullOrEmpty(event_type))
-        {
-            return event_type;
-        }
-
-        if (!string.IsNullOrEmpty(@event))
-        {
-            return @event;
-        }
-
-        return "";
+        return "message_type=" + MessageType;
     }
-}
-
-[Serializable]
-public class GuiEventTouchEnvelope
-{
-    public string message_type;
-    public GuiEventTouchPayload payload;
-}
-
-[Serializable]
-public class GuiEventHvacPayload
-{
-    public string disp_mode;
-    public string result;
-    public string value;
-}
-
-[Serializable]
-public class GuiEventHvacEnvelope
-{
-    public string message_type;
-    public GuiEventHvacPayload payload;
 }
