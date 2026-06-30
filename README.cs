8e5edd8f367d675120fcee0ba7cd27636a71c2b4
Hui Shan <61934115+huiishan99@users.noreply.github.com>
2026-06-30 13:39:36 +0900

    Update README.cs

--- README.cs
+++ README.cs
@@ -1,154 +1,63 @@
 using UnityEngine;
 
-[ExecuteAlways]
-[RequireComponent(typeof(Camera))]
-public class DisplayCameraViewFitter : MonoBehaviour
+[DisallowMultipleComponent]
+public class CursorVisibilityController : MonoBehaviour
 {
-    [SerializeField] private RectTransform targetRect;
-    [SerializeField] private float cameraDistance = 10f;
+    [SerializeField] private bool hideCursorOnStart = true;
+    [SerializeField] private bool reapplyOnFocus = true;
 
-    [Header("Direction")]
-    [SerializeField] private bool invertForward = false;
+    [Header("Debug")]
+    [SerializeField] private bool allowToggleWithKey = true;
+    [SerializeField] private KeyCode toggleKey = KeyCode.F12;
 
-    [Header("Output Rotation")]
-    [SerializeField] private bool rotateOutput180 = false;
-
-    [Tooltip("0 / 90 / 180 / 270 only. Normally use 0 or 180.")]
-    [SerializeField] private int outputRotationDegrees = 0;
-
-    [Header("Update")]
-    [SerializeField] private bool fitOnLateUpdate = true;
-
-    private Camera targetCamera;
-    private readonly Vector3[] worldCorners = new Vector3[4];
+    private bool cursorHidden;
 
     private void Awake()
     {
-        ResolveReferences();
-        FitNow();
+        SetCursorHidden(hideCursorOnStart);
     }
 
-    private void OnEnable()
+    private void OnApplicationFocus(bool hasFocus)
     {
-        ResolveReferences();
-        FitNow();
-    }
-
-    private void LateUpdate()
-    {
-        if (!fitOnLateUpdate)
+        if (!hasFocus)
         {
             return;
         }
 
-        FitNow();
-    }
-
-    public void SetTargetRect(RectTransform nextTarget)
-    {
-        targetRect = nextTarget;
-        FitNow();
-    }
-
-    public void FitNow()
-    {
-        ResolveReferences();
-
-        if (targetCamera == null || targetRect == null)
+        if (!reapplyOnFocus)
         {
             return;
         }
 
-        targetRect.GetWorldCorners(worldCorners);
+        ApplyCursorState();
+    }
 
-        Vector3 bottomLeft = worldCorners[0];
-        Vector3 topLeft = worldCorners[1];
-        Vector3 topRight = worldCorners[2];
-        Vector3 bottomRight = worldCorners[3];
-
-        Vector3 center = (bottomLeft + topRight) * 0.5f;
-
-        float width = Vector3.Distance(bottomLeft, bottomRight);
-        float height = Vector3.Distance(bottomLeft, topLeft);
-
-        if (width <= 0.001f || height <= 0.001f)
+    private void Update()
+    {
+#if ENABLE_LEGACY_INPUT_MANAGER
+        if (!allowToggleWithKey)
         {
             return;
         }
 
-        Vector3 lookDirection = invertForward
-            ? -targetRect.forward
-            : targetRect.forward;
-
-        transform.position = center - lookDirection.normalized * cameraDistance;
-
-        Quaternion baseRotation = Quaternion.LookRotation(lookDirection, targetRect.up);
-        float roll = GetNormalizedOutputRotation();
-        transform.rotation = baseRotation * Quaternion.Euler(0f, 0f, roll);
-
-        targetCamera.orthographic = true;
-
-        float cameraAspect = targetCamera.aspect;
-
-        if (cameraAspect <= 0.001f)
+        if (Input.GetKeyDown(toggleKey))
         {
-            cameraAspect = width / height;
+            SetCursorHidden(!cursorHidden);
         }
-
-        float fitWidth = width;
-        float fitHeight = height;
-
-        if (IsRightAngleRotation(roll))
-        {
-            fitWidth = height;
-            fitHeight = width;
-        }
-
-        float targetAspect = fitWidth / fitHeight;
-
-        if (cameraAspect >= targetAspect)
-        {
-            targetCamera.orthographicSize = fitHeight * 0.5f;
-        }
-        else
-        {
-            targetCamera.orthographicSize = fitWidth / (2f * cameraAspect);
-        }
+#endif
     }
 
-    private float GetNormalizedOutputRotation()
+    public void SetCursorHidden(bool hidden)
     {
-        if (rotateOutput180)
-        {
-            return 180f;
-        }
-
-        int normalized = outputRotationDegrees % 360;
-
-        if (normalized < 0)
-        {
-            normalized += 360;
-        }
-
-        if (normalized == 90 || normalized == 180 || normalized == 270)
-        {
-            return normalized;
-        }
-
-        return 0f;
+        cursorHidden = hidden;
+        ApplyCursorState();
     }
 
-    private bool IsRightAngleRotation(float rotation)
+    private void ApplyCursorState()
     {
-        return Mathf.Approximately(rotation, 90f)
-            || Mathf.Approximately(rotation, 270f);
-    }
+        Cursor.visible = !cursorHidden;
 
-    private void ResolveReferences()
-    {
-        if (targetCamera == null)
-        {
-            targetCamera = GetComponent<Camera>();
-        }
+        // Locked はWindowsキーやデバッグ操作に悪影響が出やすいため使わない。
+        Cursor.lockState = CursorLockMode.None;
     }
 }
